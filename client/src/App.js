0bd24a289a724e41674d221c3f2f9733c34a20d5
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-11 21:36:04 +0800

    Add a brief description for the post

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div class="post">
         <img src="https://keegomobility.com/wp-content/uploads/2021/01/green-transportation-electric-car.png" alt="Electric Car"/>
         <h2>Green Transportation is the Key</h2>
-
+        <p>Green transportation is any means of travel that doesn’t negatively impact the environment.</p>
       </div>
     </main>
   );
