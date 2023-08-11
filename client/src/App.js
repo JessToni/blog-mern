d994ea00bd495932c9b76727a8d02ec8a66e4d16
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-11 21:37:12 +0800

    Duplicate post three times

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,16 @@ function App() {
         <h2>Green Transportation is the Key</h2>
         <p>Green transportation is any means of travel that doesn’t negatively impact the environment.</p>
       </div>
+      <div class="post">
+        <img src="https://keegomobility.com/wp-content/uploads/2021/01/green-transportation-electric-car.png" alt="Electric Car"/>
+        <h2>Green Transportation is the Key</h2>
+        <p>Green transportation is any means of travel that doesn’t negatively impact the environment.</p>
+      </div>
+      <div class="post">
+        <img src="https://keegomobility.com/wp-content/uploads/2021/01/green-transportation-electric-car.png" alt="Electric Car"/>
+        <h2>Green Transportation is the Key</h2>
+        <p>Green transportation is any means of travel that doesn’t negatively impact the environment.</p>
+      </div>
     </main>
   );
 }
