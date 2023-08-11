80fc947462a0404fe148469506d7ec31717d2bd4
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-11 22:29:24 +0800

    Classify post description as .summary

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
             <a class="author">Jess Antoni Mosquera</a>
             <time>2023-08-11 22:21</time>
           </p>
-          <p>Green transportation is any means of travel that doesn’t negatively impact the environment.</p>
+          <p class="summary" >Green transportation is any means of travel that doesn’t negatively impact the environment.</p>
         </div>
       </div>
       <div class="post">
