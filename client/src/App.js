348733a0d5f5af0fc7a8a8c66221f7b621f42d6a
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-12 15:31:49 +0800

    Undo routing back to when the code was working

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,15 @@
 import './App.css';
 import Post from './Post';
 import Header from './Header';
-import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Routes>
-      <Route index element = {
-          <main>
-            <Header />
-            <Post />
-            <Post />
-            <Post />
-          </main>
-      } />
-    </Routes>
+        <main>
+          <Header />
+          <Post />
+          <Post />
+          <Post />
+        </main>
   );
 }
 
