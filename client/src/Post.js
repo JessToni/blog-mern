9d6fe49184167e493d315244ec47b18f9615980a
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-12 13:06:36 +0800

    Cut "post" to separate Post.js file

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return(
+        <div class="post">
+        <div class="image">
+          <img src="https://keegomobility.com/wp-content/uploads/2021/01/green-transportation-electric-car.png" alt="Electric Car"/>
+        </div>
+        <div class="text">
+          <h2>Green Transportation is the Key</h2>
+          <p class="info">
+            <a class="author">Jess Antoni Mosquera</a>
+            <time>2023-08-11 22:21</time>
+          </p>
+          <p class="summary" >Green transportation is any means of travel that doesn’t negatively impact the environment.</p>
+        </div>
+      </div>
+    );
+}
