490f73bdb70d00df0886c4371844d433793abbef
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-12 13:13:18 +0800

    Import Header from Header.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Post from './Post';
+import Header from './Header';
 
 function App() {
   return (
     <main>
+      <Header />
       <Post />
       <Post />
       <Post />
