b3377fa99cb32a0837700a141b302eaac0c7e24d
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-12 13:17:31 +0800

    Nest main element inside Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Post from './Post';
 import Header from './Header';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <main>
-      <Header />
-      <Post />
-      <Post />
-      <Post />
-    </main>
+    <Routes>
+      <Route index element = {
+          <main>
+            <Header />
+            <Post />
+            <Post />
+            <Post />
+          </main>
+      } />
+    </Routes>
   );
 }
 
