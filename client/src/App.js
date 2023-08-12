5b2bbcde0e08697c07e333abfcab37125ca6fb78
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-12 13:12:11 +0800

    Cut header to Header.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,8 @@ import Post from './Post';
 function App() {
   return (
     <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
+      <Post />
+      <Post />
       <Post />
     </main>
   );
