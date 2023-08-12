a38f149cfd8cf41c846fc3ae770396625a6d1381
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-12 15:33:00 +0800

    Change "class" to "className" in Post.js

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,16 +1,16 @@
 export default function Post() {
     return(
-        <div class="post">
-        <div class="image">
+        <div className="post">
+        <div className="image">
           <img src="https://keegomobility.com/wp-content/uploads/2021/01/green-transportation-electric-car.png" alt="Electric Car"/>
         </div>
-        <div class="text">
+        <div className="text">
           <h2>Green Transportation is the Key</h2>
-          <p class="info">
-            <a class="author">Jess Antoni Mosquera</a>
+          <p className="info">
+            <a className="author">Jess Antoni Mosquera</a>
             <time>2023-08-11 22:21</time>
           </p>
-          <p class="summary" >Green transportation is any means of travel that doesn’t negatively impact the environment.</p>
+          <p className="summary" >Green transportation is any means of travel that doesn’t negatively impact the environment.</p>
         </div>
       </div>
     );
